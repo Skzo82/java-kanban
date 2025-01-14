90e08bffeb66d8c2029fd11c0cfadb5bc89eb965
Skzo <francesco@friargiu.ru>
2025-01-14 20:14:21 +0300

    sprint 7

--- src/taskmanager/managers/TaskManager.java
+++ src/taskmanager/managers/TaskManager.java
@@ -6,26 +6,40 @@ import java.util.List;
 
 public interface TaskManager {
     Task getTask(int id);
+
     Task getSubtask(int id);
+
     Task getEpic(int id);
+
     List<Task> getTasks();
+
     List<Task> getEpics();
-    List<Task> getSubtasks();
+
     List<Subtask> getSubtasksByEpic(int epicId);
+
     List<Task> getHistory();
 
     int addNewTask(Task task);
+
     int addNewEpic(Epic epic);
+
     int addNewSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void removeTask(int id);
+
     void removeEpic(int id);
+
     void removeSubtask(int id);
+
     void removeAllTasks();
+
     void removeAllEpics();
+    
     void removeAllSubtasks();
 }
